c0ca0cd4033a9b7e86c563dfe1210f6a2d6c72e2
sonikki <esko.perkko@gmail.com>
2023-10-30 22:56:08 +0200

    1.1. bootstrap and some comments and css

--- controller.py
+++ controller.py
@@ -8,8 +8,10 @@ df = pd.read_csv('UDisc Scorecards.csv')
 
 # Replace all null values in the DataFrame with 0 and convert the 'Päivämäärä' column to a datetime object
 df = df.where(pd.notnull(df), 0)
-df['Päivämääärä'] = pd.to_datetime(df['Päivämäärä'])
+df['Päivämäärä'] = pd.to_datetime(df['Päivämäärä'])
 df['tulos'] = df['+/-']
+
+# Create a dictionary that maps each course name to a list of all layout names for that course
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -17,24 +19,25 @@ def index():
     player_names = df.loc[df['PlayerName'] != 'Par', 'PlayerName'].unique().tolist()
     player_names.insert(0, 'All Players') # Add an option for selecting all players
     course_names = df.loc[df['CourseName'].notnull(), 'CourseName'].unique().tolist()
-
-    # If the user has submitted the form, retrieve the selected player and course names
+    course_names.sort() # Sort the course names alphabetically
+    player_names.sort() # Sort the player names alphabetically
+    # If the user has submitted the form, retrieve the selected player, course, and layout names
     if request.method == 'POST':
         selected_player = request.form['player']
         selected_course = request.form['course']
 
-        # Filter the DataFrame to only include rounds played by the selected player on the selected course
+        # Filter the DataFrame to only include rounds played by the selected player on the selected course and layout
         if selected_player == 'All Players':
-            # If the user selected "All Players," only filter by the selected course
+            # If the user selected "All Players," only filter by the selected course and layout
             filtered_df = df.loc[(df['CourseName'] == selected_course) & (df['PlayerName'] != 'Par')]
         else:
             # Filter the DataFrame as usual
             filtered_df = df.loc[(df['PlayerName'] == selected_player) & (df['CourseName'] == selected_course) & (df['PlayerName'] != 'Par')]
 
-        # Get the top 10 scores for the selected player on the selected course
+        # Get the top 10 scores for the selected player on the selected course and layout
         top10_scores = filtered_df.nsmallest(10, 'tulos')
 
-        # Render the template with the selected player and course names and the top 10 scores
+        # Render the template with the selected player, course, and layout names and the top 10 scores
         return render_template('index.html', player_names=player_names, course_names=course_names, selected_player=selected_player, selected_course=selected_course, top10_scores=top10_scores)
 
     # If the user has not submitted the form, pass an empty DataFrame to the 'top10_scores' variable
