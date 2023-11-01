d016955ea2008e35f3ee2cdf403fea6fc0792644
sonikki <esko.perkko@gmail.com>
2023-11-01 14:02:28 +0200

    implement database and minor error handling

--- controller.py
+++ controller.py
@@ -1,49 +1,79 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///scorecards.db'
+db = SQLAlchemy(app)
 
-# Read in the CSV file 'UDisc Scorecards.csv' into a pandas DataFrame called 'df'
-df = pd.read_csv('UDisc Scorecards.csv')
+class Scorecard(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    player_name = db.Column(db.String(50))
+    course_name = db.Column(db.String(50))
+    date = db.Column(db.DateTime)
+    score = db.Column(db.Integer)
+    kaikki = db.Column(db.Integer)
 
-# Replace all null values in the DataFrame with 0 and convert the 'Päivämäärä' column to a datetime object
-df = df.where(pd.notnull(df), 0)
-df['Päivämäärä'] = pd.to_datetime(df['Päivämäärä'])
-df['tulos'] = df['+/-']
+    def __init__(self, player_name, course_name, date, score, kaikki):
+        self.player_name = player_name
+        self.course_name = course_name
+        self.date = date
+        self.score = score
+        self.kaikki = kaikki
 
-# Create a dictionary that maps each course name to a list of all layout names for that course
+# Load and process the data
+def load_data():
+    if Scorecard.query.first() is None:
+        try:
+            df = pd.read_csv('UDisc Scorecards.csv')
+
+            # Convert 'Päivämäärä' column to datetime
+            df['Päivämäärä'] = pd.to_datetime(df['Päivämäärä'])
+
+            for _, row in df.iterrows():
+                scorecard = Scorecard(row['PlayerName'], row['CourseName'], row['Päivämäärä'], row['+/-'], row['Kaikki'])
+                db.session.add(scorecard)
+
+            db.session.commit()
+        except FileNotFoundError:
+            print("Error: File not found")
+
+def remove_duplicates():
+    duplicates = db.session.query(Scorecard.player_name, Scorecard.course_name, Scorecard.date, Scorecard.score).group_by(Scorecard.player_name, Scorecard.course_name, Scorecard.date, Scorecard.score).having(db.func.count() > 1).all()
+    for duplicate in duplicates:
+        dup_records = Scorecard.query.filter_by(player_name=duplicate.player_name, course_name=duplicate.course_name, date=duplicate.date, score=duplicate.score).all()
+        for record in dup_records[1:]:
+            db.session.delete(record)
+    db.session.commit()
+
+with app.app_context():
+    db.create_all()
+    load_data()
+    remove_duplicates()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # Get a list of all unique player and course names in the DataFrame
-    player_names = df.loc[df['PlayerName'] != 'Par', 'PlayerName'].unique().tolist()
-    player_names.insert(0, 'All Players') # Add an option for selecting all players
-    course_names = df.loc[df['CourseName'].notnull(), 'CourseName'].unique().tolist()
-    course_names.sort() # Sort the course names alphabetically
-    player_names.sort() # Sort the player names alphabetically
-    # If the user has submitted the form, retrieve the selected player, course, and layout names
+    player_names = Scorecard.query.with_entities(Scorecard.player_name).distinct().all()
+    course_names = Scorecard.query.with_entities(Scorecard.course_name).distinct().all()
+
+    top10_scores = []
+    message = ''
+
     if request.method == 'POST':
         selected_player = request.form['player']
         selected_course = request.form['course']
 
-        # Filter the DataFrame to only include rounds played by the selected player on the selected course and layout
         if selected_player == 'All Players':
-            # If the user selected "All Players," only filter by the selected course and layout
-            filtered_df = df.loc[(df['CourseName'] == selected_course) & (df['PlayerName'] != 'Par')]
+            top10_scores = Scorecard.query.filter(Scorecard.score.isnot(None), Scorecard.course_name == selected_course).order_by(Scorecard.score).limit(10).all()
         else:
-            # Filter the DataFrame as usual
-            filtered_df = df.loc[(df['PlayerName'] == selected_player) & (df['CourseName'] == selected_course) & (df['PlayerName'] != 'Par')]
+            top10_scores = Scorecard.query.filter(Scorecard.score.isnot(None), Scorecard.player_name == selected_player, Scorecard.course_name == selected_course).order_by(Scorecard.score).limit(10).all()
 
-        # Get the top 10 scores for the selected player on the selected course and layout
-        top10_scores = filtered_df.nsmallest(10, 'tulos')
+        # Convert top10_scores to a list of dictionaries
+        top10_scores = [scorecard.__dict__ for scorecard in top10_scores]
+        if not top10_scores:
+            message = 'No scores found for ' + selected_player + ' at ' + selected_course
 
-        # Render the template with the selected player, course, and layout names and the top 10 scores
-        return render_template('index.html', player_names=player_names, course_names=course_names, selected_player=selected_player, selected_course=selected_course, top10_scores=top10_scores)
-
-    # If the user has not submitted the form, pass an empty DataFrame to the 'top10_scores' variable
-    else:
-        top10_scores = pd.DataFrame()
-        return render_template('index.html', player_names=player_names, course_names=course_names, top10_scores=top10_scores)
+    return render_template('index.html', player_names=player_names, course_names=course_names, top10_scores=top10_scores, message=message)
 
 if __name__ == '__main__':
     app.run(debug=True)
